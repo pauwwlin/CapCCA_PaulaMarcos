0ca53333a79f4fec4fcbd4fcd6cf5d80471517f9
capCCA <131385644+capCCA@users.noreply.github.com>
2025-04-09 13:34:56 +0200

    Update DniClient.java

--- src/main/java/com/capgemini/test/code/clients/DniClient.java
+++ src/main/java/com/capgemini/test/code/clients/DniClient.java
@@ -1,13 +1,32 @@
 package com.capgemini.test.code.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-@FeignClient(name = "checkDniClient", url = "${external.service.url}")
+ 
+import feign.Logger;
+import feign.okhttp.OkHttpClient;
+ 
+@FeignClient(name = "checkDniClient", url = "${external.service.url}", configuration = com.capgemini.test.code.clients.DniClient.FeignConfig.class)
 public interface DniClient {
-
-  @PatchMapping("/check-dni")
+  @Configuration
+  public class FeignConfig {
+ 
+    @Bean(name = "customOkHttpClient")
+    public OkHttpClient okHttpClient() {
+      return new OkHttpClient();
+    }
+ 
+    @Bean(name = "customLogger")
+    public Logger.Level feignLoggerLevel() {
+      return Logger.Level.FULL; // Puedes ajustar el nivel de los logs según tu necesidad
+    }
+  }
+ 
+  @PatchMapping(value = "/check-dni")
   ResponseEntity<CheckDniResponse> check(@RequestBody CheckDniRequest request);
 }
+ 
